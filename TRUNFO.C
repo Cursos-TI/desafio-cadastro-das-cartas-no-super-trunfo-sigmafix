2ce69582952bdbc18b1006a4a70bd0d23350c2f1
sigmafix <adrianojiu4x4@gmail.com>
2025-03-18 23:30:57 +0000

    criando estrutura inicial SUPER TRUNFO

---
+++ TRUNFO.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// iniciando o projeto
+// iniciando a programação
+
+int main(){
+//COLOCANDO TITULO DO PROJETO
+
+printf("desafio iniciante\n");
+printf("SUPER TRUNFO\n");
+printf("TEMAS PAÍSES");
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
